807dbcf3f4c8ce02364965fc138d3682c07956f0
Liam <lstokes@qa.com>
2020-06-09 15:31:34 +0000

    Stage 6

--- flask-blog/application/routes.py
+++ flask-blog/application/routes.py
@@ -1,10 +1,13 @@
-from flask import render_template
-from application import app
+from flask import render_template, url_for, redirect
+from application import app, db, bcrypt
+from application.models import Posts, Users
+from application.forms import PostForm, RegistrationForm
 
 @app.route('/home')
 @app.route('/')
 def home():
-    return render_template('home.html', title='Home')
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
 
 @app.route('/about')
 def about():
@@ -14,6 +17,33 @@ def about():
 def login():
     return render_template('login.html',title='Login', desc="Log in:")
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html',title='Register', desc="Register Here:")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_pw = bcrypt.generate_password_hash(form.password.data)
+
+        user = Users(email=form.email.data, password=hash_pw)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('post'))
+    return render_template('register.html',title='Register', form=form)
+
+@app.route('/post', methods=['GET','POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        new_post = Posts(
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                title=form.title.data,
+                content=form.content.data
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('post.html',title='post', form=form)
+
+
